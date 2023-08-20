e381406b578af2ea5c687099d1cf013a238f9576
prince-9999 <140087252+prince-9999@users.noreply.github.com>
2023-08-20 19:23:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-let crossbtn=document.querySelector(".cross-btn");
-let openbtn=document.querySelector(".open-btn");
-let aside=document.querySelector(".aside");
-let pophead=document.querySelector('.pop_head')
+let crossbtn = document.querySelector(".cross-btn");
+let openbtn = document.querySelector(".open-btn");
+let aside = document.querySelector(".aside");
+let pophead = document.querySelector(".pop_head");
+let clear_btn = document.querySelector(".clear_btn");
+let bookmark_ul = document.querySelector(".bookmark_ul");
 
 //   ----------(opening the sidebar)---------------
 
-function sidebar(){
-  aside.style.left="0.1rem"
-  openbtn.style.visibility="hidden"
- 
+function sidebar() {
+  aside.style.left = "0.1rem";
+  openbtn.style.visibility = "hidden";
+  aside.style.display = "unset";
 }
-
-function disp(){
-  aside.style.left="-16rem"
-  openbtn.style.visibility="visible"
-}
-
 
 // --------------------save_ul---------------------------
 
@@ -23,7 +19,7 @@ function disp(){
 //   arrdata.forEach((elem) => {
 //     let li=document.createElement('li'); //creation of li
 //     li.innerHTML=`+`
-//     save_ul.appendChild(li); 
+//     save_ul.appendChild(li);
 //   });
 
 //   let save_li=document.querySelectorAll('.save_ul li')[0]
@@ -34,87 +30,84 @@ function disp(){
 //   })
 
 //   ----------------------(bookmarking)--------------------
+// // var arrdata;
+// <script src="api.js"></script>;
 
-let prac_ul=document.querySelector('.prac_ul')
-  arrdata.forEach((elem) => {
-  let li=document.createElement('li'); //creation of li
-  li.innerHTML=`${elem.prob}`
-  prac_ul.appendChild(li); 
- 
+let prac_ul = document.querySelector(".prac_ul");
+arrdata.forEach((elem) => {
+  let li = document.createElement("li"); //creation of li
+  li.innerHTML = `${elem.prob}`;
+  prac_ul.appendChild(li);
 });
 
- let i=0;
+let i = 0;
 
+let click = document.querySelectorAll(".prac_ul li");
 
-  let click=document.querySelectorAll('.prac_ul li');
-  
-  click.forEach((elem)=>{
-      elem.addEventListener("click",(e)=>{
-      console.log(e)
-      setTimeout((e)=>{
+click.forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    console.log(e);
+    setTimeout((e) => {
       window.location.reload();
-      },2000)
-      localStorage.setItem(`${Date.now()}`,`${ click[0].innerHTML}`)// key value pair
-      pops("bookmarked successfully")
-     })
-  })
- 
-
+    }, 2000);
+    localStorage.setItem(`${Date.now()}`, `${click[0].innerHTML}`); // key value pair
+    pops("bookmarked successfully");
+  });
+});
 
 //    localStorage.clear();
- function clears(){
-    pops("list cleared successfully")
-    localStorage.clear()
-    setTimeout(()=>{
-      window.location.reload()
-    },3000)
-    
- }
+function clears() {
+  pops("list cleared successfully");
+  localStorage.clear();
+  setTimeout(() => {
+    window.location.reload();
+  }, 3000);
+}
 
 // how to foreach over localstorage
-let bookmark=document.querySelector('.bookmark_ul')
+let bookmark = document.querySelector(".bookmark_ul");
 
- Object.keys(localStorage).forEach((key)=>{
-  let li2=document.createElement('li'); 
-  li2.innerHTML=`${localStorage.getItem(key)}`
+Object.keys(localStorage).forEach((key) => {
+  let li2 = document.createElement("li");
+  li2.innerHTML = `${localStorage.getItem(key)}`;
   bookmark.appendChild(li2);
- })
-
+});
 
 //    ---------****-------serach_filter--------------
 
-const searchfilter=()=>{
-  let inputfield=document.querySelector('.input_field').value.toUpperCase();
-  
-  let prac_ul=document.querySelector('.prac_ul')
-  let li_item=prac_ul.getElementsByTagName('li');        
-  for(let i=0;i<li_item.length;i++){
+const searchfilter = () => {
+  let inputfield = document.querySelector(".input_field").value.toUpperCase();
 
-      let currli=li_item[i]
-       if(currli){
-  
-          let textvalue=currli.textContent|| currli.innerHTML;
-          if(textvalue.toUpperCase().indexOf(inputfield)>-1){
-              li_item[i].style.display="";
-          }else{
-              li_item[i].style.display="none";
-          }
-        
-       }
+  let prac_ul = document.querySelector(".prac_ul");
+  let li_item = prac_ul.getElementsByTagName("li");
+  for (let i = 0; i < li_item.length; i++) {
+    let currli = li_item[i];
+    if (currli) {
+      let textvalue = currli.textContent || currli.innerHTML;
+      if (textvalue.toUpperCase().indexOf(inputfield) > -1) {
+        li_item[i].style.display = "";
+      } else {
+        li_item[i].style.display = "none";
+      }
+    }
   }
-}
+};
 
 // -----------------((POPUP))-----------------------
 
-let sidepop=document.querySelector('.pop-box')
-function pops(props){
+let sidepop = document.querySelector(".pop-box");
+function pops(props) {
   // let sidepop_after=window.getComputedStyle(sidepop,'::after')
-    pophead.innerHTML=props
-    sidepop.style.left="0"
-    let timer=setTimeout(()=>{
-      sidepop.style.left="-21rem"
-     },2500)
+  pophead.innerHTML = props;
+  sidepop.style.left = "0";
+  let timer = setTimeout(() => {
+    sidepop.style.left = "-21rem";
+  }, 2500);
 
-  sidepop.style.setProperty('--scaler','scale(0,1)')
+  sidepop.style.setProperty("--scaler", "scale(0,1)");
 }
-
+function disp() {
+  aside.style.left = "-16rem";
+  openbtn.style.visibility = "visible";
+  aside.style.display = "none";
+}
